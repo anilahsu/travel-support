58863ef5e12e6b15e7795c3da83b7beebefc123a
simple1017 <simple01281017@gmail.com>
2023-08-07 10:33:11 +0800

    fix: reset the next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return  [
-      {
-        source: '/',
-        destination: `https://www.pocket-change.jp/userguide`,
-      },
-    ]
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
