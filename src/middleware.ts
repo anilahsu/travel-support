fd659510a71ddf379243c2e2dc457d3fcbaa0f40
simple1017 <simple01281017@gmail.com>
2023-08-08 15:49:01 +0800

    fix: excluding the font of public in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,18 +23,23 @@ function getLocale(request: NextRequest): string | undefined {
 }
 
 export function middleware(request: NextRequest) {
+  console.log(request.url);
   const pathname = request.nextUrl.pathname
 
   // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
   // // If you have one
-  // if (
-  //   [
-  //     '/manifest.json',
-  //     '/favicon.ico',
-  //     // Your other files in `public`
-  //   ].includes(pathname)
-  // )
-  //   return
+  if (
+    [
+      '/manifest.json',
+      '/favicon.ico',
+      // Your other files in `public`
+      '/fonts/NotoSansCJKjp-Regular.woff2',
+      '/fonts/NotoSansCJKjp-Regular.woff',
+      '/fonts/NotoSansCJKjp-Bold.woff',
+      '/fonts/NotoSansCJKjp-Bold.woff2',
+    ].includes(pathname)
+  )
+    return
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
