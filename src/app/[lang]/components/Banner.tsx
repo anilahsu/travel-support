73fb252be649c7d7cf2f5513b5510f509aa4b1f9
simple1017 <simple01281017@gmail.com>
2023-08-08 16:33:45 +0800

    feat: add banner component

---
+++ src/app/[lang]/components/Banner.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "@/app/page.module.css";
+import kiosk from "/public/images/kiosk.png";
+import { Dictionary } from "@/type";
+
+const Banner = ({ dictionary }: { dictionary: Dictionary }) => {
+  return (
+    <div className={styles.banner}>
+      <Image
+        src={kiosk}
+        alt={dictionary.kioskImage.alt}
+        className={styles.kioskImage}
+      />
+    </div>
+  );
+};
+
+export default Banner;
