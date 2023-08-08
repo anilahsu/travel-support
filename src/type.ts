83766474b772cccb8318a18e02297e7bdf2ff978
simple1017 <simple01281017@gmail.com>
2023-08-08 16:30:45 +0800

    feat: add type for dictionary

---
+++ src/type.ts
@@ -0,0 +1,22 @@
+export type Dictionary = {
+  logo: {
+    alt: string;
+  };
+  globeIcon: {
+    alt: string;
+  };
+  kioskImage: {
+    alt: string;
+  };
+  menuList: {
+    icon: {
+      src: string;
+      alt: string;
+    };
+    title: string;
+    description: string;
+  }[];
+  chevron: {
+    alt: string;
+  };
+};
