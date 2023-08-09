e4876ca73fd867271e9a52a0a1f3e2d1eec4b4c4
simple1017 <simple01281017@gmail.com>
2023-08-09 10:06:08 +0800

    feat: support images with dark mode for Header component

--- src/app/[lang]/components/Header.tsx
+++ src/app/[lang]/components/Header.tsx
@@ -1,20 +1,34 @@
 import Image from "next/image";
 import styles from "@/app/page.module.css";
 import logo from "/public/images/logo.png";
+import logoDarkMode from "/public/images/logo-dark-mode.png";
 import globeIcon from "/public/images/globe.svg";
+import globeIconDarkMode from "/public/images/globe-dark-mode.svg";
 import { Dictionary } from "@/type";
 
 const Header = ({ dictionary }: { dictionary: Dictionary }) => {
   return (
     <div className={styles.header}>
-      <Image src={logo} alt={dictionary.logo.alt} className={styles.logo} />
-      <div className={styles.langButton}>
-        <Image
-          src={globeIcon}
-          alt={dictionary.globeIcon.alt}
-          width={20}
-          height={20}
+      <picture className={styles.imageWrapper}>
+        <source
+          srcSet={logoDarkMode.src}
+          media="(prefers-color-scheme: dark)"
         />
+        <Image src={logo} alt={dictionary.logo.alt} className={styles.logo} />
+      </picture>
+      <div className={styles.langButton}>
+        <picture className={styles.imageWrapper}>
+          <source
+            srcSet={globeIconDarkMode.src}
+            media="(prefers-color-scheme: dark)"
+          />
+          <Image
+            src={globeIcon}
+            alt={dictionary.globeIcon.alt}
+            width={20}
+            height={20}
+          />
+        </picture>
         Language
       </div>
     </div>
