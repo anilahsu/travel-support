4ec767132b67e00d821610b706ef11ecd36ea5f9
simple1017 <simple01281017@gmail.com>
2023-08-09 10:08:08 +0800

    feat: support chevron with dark mode for Menu component

--- src/app/[lang]/components/Menu.tsx
+++ src/app/[lang]/components/Menu.tsx
@@ -1,38 +1,44 @@
 import Image from "next/image";
 import styles from "@/app/page.module.css";
 import { Dictionary } from "@/type";
-import chevron from "/public/images/chevron-right.svg";
-
+import chevron from "/public/images/chevron.svg";
+import chevronDarkMode from "/public/images/chevron-dark-mode.svg";
 
 const Menu = ({ dictionary }: { dictionary: Dictionary }) => {
   return (
     <div className={styles.menuContainer}>
-          {dictionary.menuList.map((item, index) => {
-            return (
-              <div key={index} className={styles.menuItem}>
-                <div className={styles.menuContent}>
-                  <span className={styles.iconContainer}>
-                    <Image
-                      src={item.icon.src}
-                      alt={item.icon.alt}
-                      width={24}
-                      height={24}
-                    />
-                  </span>
-                  <p className={styles.itemTitle}>{item.title}</p>
-                  <p className={styles.itemDescription}>{item.description}</p>
-                </div>
+      {dictionary.menuList.map((item, index) => {
+        return (
+          <div key={index} className={styles.menuItem}>
+            <div className={styles.menuContent}>
+              <span className={styles.iconContainer}>
                 <Image
-                  src={chevron}
-                  alt={dictionary.chevron.alt}
+                  src={item.icon.src}
+                  alt={item.icon.alt}
                   width={24}
                   height={24}
                 />
-              </div>
-            );
-          })}
-        </div>
-  )
-}
+              </span>
+              <p className={styles.itemTitle}>{item.title}</p>
+              <p className={styles.itemDescription}>{item.description}</p>
+            </div>
+            <picture className={styles.imageWrapper}>
+              <source
+                srcSet={chevronDarkMode.src}
+                media="(prefers-color-scheme: dark)"
+              />
+              <Image
+                src={chevron}
+                alt={dictionary.chevron.alt}
+                width={24}
+                height={24}
+              />
+            </picture>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
