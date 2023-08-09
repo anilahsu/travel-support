8198403a7292fbac5b6df8a359daf88e85ceb68e
simple1017 <simple01281017@gmail.com>
2023-08-09 11:01:55 +0800

    feat: componentize the logo

--- src/app/[lang]/components/Header.tsx
+++ src/app/[lang]/components/Header.tsx
@@ -1,21 +1,14 @@
 import Image from "next/image";
 import styles from "@/app/page.module.css";
-import logo from "/public/images/logo.png";
-import logoDarkMode from "/public/images/logo-dark-mode.png";
 import globeIcon from "/public/images/globe.svg";
 import globeIconDarkMode from "/public/images/globe-dark-mode.svg";
 import { Dictionary } from "@/type";
+import Logo from "./Logo";
 
 const Header = ({ dictionary }: { dictionary: Dictionary }) => {
   return (
     <div className={styles.header}>
-      <picture className={styles.imageWrapper}>
-        <source
-          srcSet={logoDarkMode.src}
-          media="(prefers-color-scheme: dark)"
-        />
-        <Image src={logo} alt={dictionary.logo.alt} className={styles.logo} />
-      </picture>
+      <Logo dictionary={dictionary} />
       <div className={styles.langButton}>
         <picture className={styles.imageWrapper}>
           <source
---
+++ src/app/[lang]/components/Logo.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "@/app/page.module.css";
+import logo from "/public/images/logo.png";
+import logoDarkMode from "/public/images/logo-dark-mode.png";
+import { Dictionary } from "@/type";
+
+const Logo = ({ dictionary }: { dictionary: Dictionary }) => {
+  return (
+    <picture className={styles.imageWrapper}>
+      <source srcSet={logoDarkMode.src} media="(prefers-color-scheme: dark)" />
+      <Image src={logo} alt={dictionary.logo.alt} className={styles.logo} />
+    </picture>
+  );
+};
+
+export default Logo;
