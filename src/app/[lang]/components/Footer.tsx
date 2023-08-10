919c2412f2b51ea9c1194e2791b1af0db25818d1
simple1017 <simple01281017@gmail.com>
2023-08-10 11:51:45 +0800

    feat: add footer component

---
+++ src/app/[lang]/components/Footer.tsx
@@ -0,0 +1,23 @@
+import styles from "@/app/page.module.css";
+import { Dictionary } from "@/type";
+import Logo from "./Logo";
+import Link from "next/link";
+
+const Footer = ({ dictionary }: { dictionary: Dictionary }) => {
+  return (
+    <div className={styles.footerContainer}>
+      <div className={styles.footerLogoWrapper}>
+        <Logo dictionary={dictionary} />
+      </div>
+      {dictionary.footerButtons.map((button, index) => {
+        return(
+          <Link key={index} href={button.link} className={styles.footerButton}>
+            {button.text}
+          </Link>
+        )
+      })}
+    </div>
+  );
+};
+
+export default Footer;
