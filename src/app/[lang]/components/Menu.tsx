557c5c5661ba56ddd237cf8e89948ba61d786e83
simple1017 <simple01281017@gmail.com>
2023-08-14 12:39:54 +0800

    feat: add skeleton when menu and button loading

--- src/app/[lang]/components/Menu.tsx
+++ src/app/[lang]/components/Menu.tsx
@@ -4,47 +4,57 @@ import { Dictionary } from "@/type";
 import chevron from "/public/images/chevron.svg";
 import chevronDarkMode from "/public/images/chevron-dark-mode.svg";
 import Link from "next/link";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const Menu = ({ dictionary }: { dictionary: Dictionary }) => {
   return (
     <div className={styles.menuContainer}>
-      {dictionary && dictionary.menuList.map((item, index) => {
-        return (
-          <Link key={index} href={item.link} className={styles.menuItem}>
-            <div className={styles.menuContent}>
-              <span className={styles.iconContainer}>
+      {dictionary ? (
+        dictionary.menuList.map((item, index) => {
+          return (
+            <Link key={index} href={item.link} className={styles.menuItem}>
+              <div className={styles.menuContent}>
+                <span className={styles.iconContainer}>
+                  <Image
+                    src={item.icon.src}
+                    alt={item.icon.alt}
+                    width={24}
+                    height={24}
+                  />
+                </span>
+                <p className={styles.itemTitle}>{item.title}</p>
+                <p className={styles.itemDescription}>{item.description}</p>
+              </div>
+              <picture className={styles.imageThemeWrapper}>
+                <source
+                  srcSet={chevronDarkMode.src}
+                  media="(prefers-color-scheme: dark)"
+                />
                 <Image
-                  src={item.icon.src}
-                  alt={item.icon.alt}
+                  src={chevron}
+                  alt={dictionary.chevron.alt}
                   width={24}
                   height={24}
                 />
-              </span>
-              <p className={styles.itemTitle}>{item.title}</p>
-              <p className={styles.itemDescription}>{item.description}</p>
-            </div>
-            <picture className={styles.imageThemeWrapper}>
-              <source
-                srcSet={chevronDarkMode.src}
-                media="(prefers-color-scheme: dark)"
-              />
-              <Image
-                src={chevron}
-                alt={dictionary.chevron.alt}
-                width={24}
-                height={24}
-              />
-            </picture>
-          </Link>
-        );
-      })}
-      {dictionary && dictionary.buttons.map((button, index) => {
-        return (
-          <Link key={index} href={button.link} className={styles.button}>
-            {button.text}
-          </Link>
-        );
-      })}
+              </picture>
+            </Link>
+          );
+        })
+      ) : (
+        <Skeleton height={152} count={2} />
+      )}
+      {dictionary ? (
+        dictionary.buttons.map((button, index) => {
+          return (
+            <Link key={index} href={button.link} className={styles.button}>
+              {button.text}
+            </Link>
+          );
+        })
+      ) : (
+        <Skeleton height={48} count={2} />
+      )}
     </div>
   );
 };
