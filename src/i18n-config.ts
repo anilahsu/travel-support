f17de8d3b98917391b5f0ef19133dc265aebf82a
simple1017 <simple01281017@gmail.com>
2023-11-29 15:38:16 +0900

    feat: update i18n config for zn, tw and ko language

--- src/i18n-config.ts
+++ src/i18n-config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
   defaultLocale: 'en',
-  locales: ['en', 'ja'],
+  locales: ['en', 'ja', 'zn', 'tw', 'ko'],
 } as const
 
 export type Locale = (typeof i18n)['locales'][number]
